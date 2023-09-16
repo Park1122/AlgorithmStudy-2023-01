6a38534918037e66a60829bc724d110433cfd83f
Park Jungwook <cco2416@gmail.com>
2023-09-16 12:36:21 +0900

    Week 37

---
+++ src/graphSearch/BOJ24445.java
@@ -0,0 +1,72 @@
+package graphSearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ24445 {
+    static int N, M, R;
+    static ArrayList<Integer>[] graph;
+    static int[] arr;
+    static boolean[] visit;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N + 1];
+        arr = new int[N + 1];
+        visit = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i], Collections.reverseOrder());
+        }
+    }
+
+    static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visit[start] = true;
+        int idx = 1;
+        arr[start] = idx;
+        idx++;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            for (int i : graph[now]) {
+                if(visit[i]) continue;
+                visit[i] = true;
+                arr[i] = idx;
+                q.add(i);
+                idx++;
+            }
+        }
+    }
+
+    static void pro() {
+        bfs(R);
+        for (int i = 1; i <= N; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+}
---
+++ src/graphSearch/BOJ24480.java
@@ -0,0 +1,68 @@
+package graphSearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BOJ24480 {
+    static int N, M, R, idx;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visit;
+    static int[] arr;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[N + 1];
+        visit = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        // 오름차순 방문을 위한 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i], Collections.reverseOrder());
+        }
+        arr = new int[N + 1];
+    }
+
+    static void dfs(int start) {
+        visit[start] = true;
+        arr[start] = idx;
+
+        for (int i : graph[start]) {
+            if(visit[i]) continue;
+            idx++;
+            dfs(i);
+        }
+    }
+
+    static void pro() {
+        idx = 1;
+        dfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+}
